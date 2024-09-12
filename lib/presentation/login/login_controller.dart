9cd78696db9f1f54b7da7cf6dbea488ee680e02e
Cássio Meira <cassiomeira_12@hotmail.com>
2024-09-12 15:27:43 -0300

    fixes

--- lib/presentation/login/login_controller.dart
+++ lib/presentation/login/login_controller.dart
@@ -1,9 +1,18 @@
 import 'package:flutter_demo_app/core/core.dart';
+import 'package:get/get.dart';
 
+import '../../domain/domain.dart';
 import '../app_router.dart';
 
 class LoginController extends BaseController {
   void login() {
+    Get.put<SessionEntity>(
+      SessionEntity(
+        token: '',
+      ),
+      permanent: true,
+    );
+
     AppRouter.offAndToNamed(AppRouter.home);
   }
 }
