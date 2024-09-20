214c6e7fba699e2887714735e6fee8df9c590577
Cássio Meira <cassiomeira_12@hotmail.com>
2024-09-20 10:21:34 -0300

    fix: update local storage

--- lib/data/use_cases/local_storage_use_case.dart
+++ lib/data/use_cases/local_storage_use_case.dart
@@ -4,9 +4,29 @@ import '../../domain/use_cases/use_cases.dart';
 
 class LocalStorageUseCase implements ILocalStorageUseCase {
   @override
-  Future<void> clearAll() async {
+  Future<T?> get<T>(String key) async {
     final pref = await SharedPreferences.getInstance();
-    await pref.clear();
+    return pref.get(key) as T?;
+  }
+
+  @override
+  Future<bool> set<T>(String key, T value) async {
+    final pref = await SharedPreferences.getInstance();
+
+    switch (value.runtimeType) {
+      case const (int):
+        return await pref.setInt(key, value as int);
+      case const (bool):
+        return await pref.setBool(key, value as bool);
+      case const (double):
+        return await pref.setDouble(key, value as double);
+      case const (String):
+        return await pref.setString(key, value as String);
+      case const (List<String>):
+        return await pref.setStringList(key, value as List<String>);
+      default:
+        throw '${value.runtimeType} is not a Type acceptable';
+    }
   }
 
   @override
@@ -16,27 +36,8 @@ class LocalStorageUseCase implements ILocalStorageUseCase {
   }
 
   @override
-  Future<dynamic> get(String key) async {
+  Future<void> clearAll() async {
     final pref = await SharedPreferences.getInstance();
-    return pref.get(key);
-  }
-
-  @override
-  Future<bool> set(String key, dynamic value) async {
-    final pref = await SharedPreferences.getInstance();
-    switch (value.runtimeType.toString()) {
-      case 'bool':
-        return await pref.setBool(key, value as bool);
-      case 'double':
-        return await pref.setDouble(key, value as double);
-      case 'int':
-        return await pref.setInt(key, value as int);
-      case 'String':
-        return await pref.setString(key, value as String);
-      case 'List<String>':
-        return await pref.setStringList(key, value as List<String>);
-      default:
-        throw Exception('type not accepted');
-    }
+    await pref.clear();
   }
 }
