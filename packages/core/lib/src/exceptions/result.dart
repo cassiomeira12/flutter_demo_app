16840d9e3bd833ae92cb4f53f57a07726a95d2b1
Cássio M. Silva <contato.cassiomsilva@gmail.com>
2026-06-09 16:11:58 -0300

    [master] feat: add Result success and error base functions

--- packages/core/lib/src/exceptions/result.dart
+++ packages/core/lib/src/exceptions/result.dart
@@ -2,6 +2,14 @@ import 'package:core/core.dart';
 
 abstract class Result<T> {
   const Result();
+
+  static Success<T> success<T>([T? value]) {
+    return Success<T>(value);
+  }
+
+  static Error<T> error<T>(BaseException error) {
+    return Error<T>(error);
+  }
 }
 
 class Success<T> extends Result<T> {
